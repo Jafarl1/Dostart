3122039e468fb92ef189213d001fc7db138e375d
Jafar1i <103290548+Jafarl1@users.noreply.github.com>
2022-09-24 15:29:19 +0400

    remove files

--- images/Vusal/newDosya.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function newDosya() {
-  return (
-    <div>newDosya</div>
-  )
-}
-
-export default newDosya
--- images/Zohrab/Zohrab.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Zohrab() {
-  return (
-    <div>Zohrab</div>
-  )
-}
-
-export default Zohrab
